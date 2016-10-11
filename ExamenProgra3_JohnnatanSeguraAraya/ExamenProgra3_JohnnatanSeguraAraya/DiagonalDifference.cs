71e0bad2fab5903b0a9c9dc05f53794619e54498
j0hnnatan <j_segura_araya@hotmail.com>
2016-10-10 22:21:15 -0600

    Primera subida
    problema #2 resuelto, y problema #1 en progreso

---
+++ ExamenProgra3_JohnnatanSeguraAraya/ExamenProgra3_JohnnatanSeguraAraya/DiagonalDifference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgra3_JohnnatanSeguraAraya
+{
+    public class DiagonalDifference
+    {
+        public int matrix(string cadena)
+        {
+            int resultado = 0;
+            string[] primerSplit = cadena.Split('|');
+            string[] segundoSplit;
+            int[][] matrizNumeros = new int[0][];
+            for (int i = 0; i < primerSplit.Length; i++)
+            {
+                segundoSplit = primerSplit[i].Split(' ');
+                for (int fila = 0; fila < segundoSplit.Length; fila++)
+                {
+                    for (int columna = 0; columna < segundoSplit.Length; columna++)
+                    {
+                        matrizNumeros[fila][columna] = int.Parse(segundoSplit[columna]);
+                    }
+                }               
+            }   
+                 
+
+            return resultado;
+        }
+
+    }
+}
---
+++ ExamenProgra3_JohnnatanSeguraAraya/ExamenProgra3_JohnnatanSeguraAraya/camelCase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenProgra3_JohnnatanSeguraAraya
+{
+    public class camelCase
+    {
+        public int calculate(string camelCaseWord)
+        {
+            int resultado = 0;
+
+            //evalua si el string que viene por parametro esta vacio o es nulo, esto para seguir con el resto del codigo o retornar cero
+            if (!string.IsNullOrWhiteSpace(camelCaseWord))
+            {
+                //si el string contiene almenos una palabra, 
+                //cambia la variable "resultado" a 1 porque se toma en cuanta que la primera palabra todas sus letras estan en minuscula
+                resultado = 1;
+
+                //luego un for que recorre todas las letras del string que viene por parametro
+                for (int posicion = 0; posicion < camelCaseWord.Length; posicion++)
+                {
+                    //se evalua cada letra del string, utilizando la clase 'char',
+                    //donde nos indica si la letra del string en una posicion  especifica es mayuscula o nimuscula
+                    if (char.IsUpper(camelCaseWord[posicion]))
+                    {
+                        //si la letra del string en la posicion que se evaluo es mayuscula, se incrementa la variable "resultado en uno"
+                        resultado++;
+                    }
+                }
+            }
+            return resultado;
+        }
+    }
+}
